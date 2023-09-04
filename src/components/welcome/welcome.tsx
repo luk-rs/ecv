6739a1f35fae2ebbd0e3e83c4cf6b89f62f518bc
luk-rs <santos8@gmail.com>
2023-09-04 22:51:50 +0100

    add welcome page and image, and started personal page

---
+++ src/components/welcome/welcome.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import styles from "./welcome.module.css";
+
+
+export default function Welcome() {
+
+  const inspiration = "Recomeça... se puderes, sem angústia e sem pressa e os passos que deres, nesse caminho duro do futuro, dá-os em liberdade, enquanto não alcances não descanses, de nenhum fruto queiras só metade.".split('');
+  inspiration.push('"');
+  inspiration.unshift('"');
+
+  const phrase = inspiration
+
+    .map((char, idx) => (
+      <span
+        key={`welcome-span-${idx}`}
+        style={{ animationDelay: `${.1 * idx}s` }}>
+        {char}
+      </span>
+    ));
+
+
+
+  return (
+    <div className={styles.personal}>
+      <p>{phrase}</p>
+    </div>
+  );
+}

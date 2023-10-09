6d7ce0ef10ec64f4aa4f15c2076c20d78bcd77c8
luk-rs <santos8@gmail.com>
2023-10-09 16:58:40 +0100

    moved schools await to page as components seem to cannot be async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 
+import { fetchSchools } from "@/actions/fetch-schools-action";
 import Education from '@/components/education/education';
 import Personal from '@/components/personal/personal';
 import Welcome from '@/components/welcome/welcome';
+import { School } from '@/models/school';
 import styles from './page.module.css';
 
+const dictionary: (schools: School[]) => { [key: string]: JSX.Element } = (schools) => {
+  return {
+    "welcome": <Welcome />,
+    "personal": <Personal />,
+    "education": <Education schools={schools} />
+  }
+};
+const builder = (link: string, idx: number, schools: School[]) => {
 
-const dictionary: { [key: string]: JSX.Element } = {
-  "welcome": <Welcome />,
-  "personal": <Personal />,
-  "education": <Education />
-}
-
-const builder = (link: string, idx: number) => {
-
-  const elem = dictionary[link] || link;
+  const elem = dictionary(schools)[link] || link;
   return (
     <section id={link} key={`home-section-${idx}`}>
       {elem}
@@ -21,15 +23,18 @@ const builder = (link: string, idx: number) => {
   )
 }
 
-export default function Home() {
+export default async function Home() {
 
   const splitted = process.env.HTML_SECTIONS
     ?.split(',') ?? [];
 
   const skip = 1;
 
+  const schools: School[] = await fetchSchools(); //TODO move to something that makes sense
+
   const sections = splitted
-    .map((section, idx) => builder(section, idx));
+    .map((section, idx) => builder(section, idx, schools));
+
 
   return (
     <main className={styles.main}>

bc77e42c6f1df238ad21679e746dc4b3e7d48753
luk-rs <santos8@gmail.com>
2023-10-09 16:01:48 +0100

    finished implementing education section

--- src/components/education/education.tsx
+++ src/components/education/education.tsx
@@ -1,17 +1,14 @@
-
 import { fetchSchools } from "@/actions/fetch-schools-action";
-import { School } from "@/lib/types/school";
-import styles from "./education.module.css";
+import { School } from "@/models/school";
+import EducationRenderer from "./educationRenderer";
 
 export default async function Education() {
 
-  let schools: School[] = [];
-  schools = await fetchSchools();
+  let schools: School[] = await fetchSchools();
 
   return (
-    <div className={styles.education}>
-      {schools.map(school => <>{school.name + " => " + school.level}<br /></>)}
-    </div>
+    <>
+      <EducationRenderer schools={schools} />
+    </>
   )
 }
-
